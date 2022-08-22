6da31c31f6aa5993e99f6d465b1e4a9b8bb4a666
Kevin <kevin.kipkoech@student.moringaschool.com>
2022-08-22 05:48:41 +0300

    Extra changes

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function divide (num1=2000, num2=100) {
 
 //square
 
-let square = x => {return x * x};
+let square = x =>  x * x;
 
 //add
-let add = (a, b) => {return a+b};
+let add = (a, b) =>  a+b;
